6ea013870dd872e4711362b6698a172c7a0523af
mixiujin <2423776460@qq.com>
2024-04-12 15:39:23 +0800

    int:模块初始化

---
+++ gulimall-member/src/test/java/com/mxj/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxj.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
