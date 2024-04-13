ccec6f2fc94974a8e151a76f68387808d619d1b2
mixiujin <2423776460@qq.com>
2024-04-14 01:29:54 +0800

    test:openfeign及nacos

--- gulimall-coupon/src/main/java/com/mxj/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/mxj/gulimall/coupon/GulimallCouponApplication.java
@@ -1,11 +1,11 @@
 package com.mxj.gulimall.coupon;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.mxj.gulimall.coupon.dao")
+@EnableDiscoveryClient
 public class GulimallCouponApplication {
 
     public static void main(String[] args) {
--- gulimall-member/src/main/java/com/mxj/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/mxj/gulimall/member/GulimallMemberApplication.java
@@ -2,8 +2,16 @@ package com.mxj.gulimall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * 远程调用其他服务
+ * 1.引入openfeign
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.mxj.gulimall.member.feign")
 public class GulimallMemberApplication {
 
     public static void main(String[] args) {
