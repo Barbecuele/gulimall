fcc81fac6bf553bb34200a2fe410e758c6b9aa46
mixiujin <2423776460@qq.com>
2024-04-19 00:51:54 +0800

    add:菜单删除

--- gulimall-product/src/main/java/com/mxj/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/mxj/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -39,24 +40,28 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
     public List<CategoryEntity> listWithTree() {
         //1.查出所有分类
         List<CategoryEntity> entities = categoryDao.selectList(null);
-        //2.组装成父子的树形结构
+        //2.组装成父子的树形结构777
         //2.1找到所有的一级分类
-        List<CategoryEntity> level1Menus = entities.stream().filter(categoryEntity ->
+        return entities.stream().filter(categoryEntity ->
                 categoryEntity.getParentCid() == 0
         ).peek(menu -> menu.setChildren(getChildrens(menu, entities))).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))
         ).collect(Collectors.toList());
-        return level1Menus;
+    }
+
+    @Override
+    public void removeMenuByIds(List<Long> list) {
+        //TODO 检查当前删除的菜单，是否被其他地方引用
+        categoryDao.deleteBatchIds(list);
     }
 
     /**
      * 递归查找所有菜单的子菜单
      */
     private List<CategoryEntity> getChildrens(CategoryEntity menu, List<CategoryEntity> entities) {
-        List<CategoryEntity> children = entities.stream().filter(categoryEntity ->
-                categoryEntity.getParentCid() == menu.getCatId()
+        return entities.stream().filter(categoryEntity ->
+                Objects.equals(categoryEntity.getParentCid(), menu.getCatId())
         ).peek(categoryEntity -> categoryEntity.setChildren(getChildrens(categoryEntity, entities))).sorted(Comparator.comparingInt(menu1 -> (menu1.getSort() == null ? 0 : menu1.getSort()))
         ).collect(Collectors.toList());
-        return children;
     }
 
 }
